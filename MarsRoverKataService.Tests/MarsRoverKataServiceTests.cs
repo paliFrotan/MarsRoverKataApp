8c8704a67ad4a4a5d15e44edf40c69882d720a3c
Pali Frotan <pali.frotan@icloud.com>
2022-09-19 09:05:59 +0100

    Initial commit of project

---
+++ MarsRoverKataService.Tests/MarsRoverKataServiceTests.cs
@@ -0,0 +1,15 @@
+namespace MarsRoverKataService.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
